d8d2b9be68ce7d681aba13c695413c51d24cbb31
B. Howe <37745048+byhowe@users.noreply.github.com>
2020-09-20 07:07:47 -0800

    implement the basic outline of the crate

---
+++ src/lib.rs
@@ -0,0 +1,126 @@
+use std::io;
+
+pub use struct_reader_derive::*;
+
+pub trait Reader: io::Read
+{
+  #[inline]
+  fn read<T>(&mut self) -> io::Result<T>
+  where
+    T: Read,
+    Self: Sized,
+  {
+    T::read(self)
+  }
+
+  #[inline]
+  fn read_le<T>(&mut self) -> io::Result<T>
+  where
+    T: PrimitiveRead,
+    Self: Sized,
+  {
+    T::read_le(self)
+  }
+
+  #[inline]
+  fn read_be<T>(&mut self) -> io::Result<T>
+  where
+    T: PrimitiveRead,
+    Self: Sized,
+  {
+    T::read_be(self)
+  }
+}
+
+pub trait PrimitiveRead
+{
+  fn read_le<R>(reader: &mut R) -> io::Result<Self>
+  where
+    R: io::Read,
+    Self: Sized;
+
+  fn read_be<R>(reader: &mut R) -> io::Result<Self>
+  where
+    R: io::Read,
+    Self: Sized;
+
+  #[cfg(target_endian = "little")]
+  #[inline]
+  fn read_ne<R>(reader: &mut R) -> io::Result<Self>
+  where
+    R: io::Read,
+    Self: Sized,
+  {
+    Self::read_le(reader)
+  }
+
+  #[cfg(target_endian = "big")]
+  #[inline]
+  fn read_ne<R>(reader: &mut R) -> io::Result<Self>
+  where
+    R: io::Read,
+    Self: Sized,
+  {
+    Self::read_be(reader)
+  }
+}
+
+pub trait Read
+{
+  fn read<R>(reader: &mut R) -> io::Result<Self>
+  where
+    R: io::Read,
+    Self: Sized;
+}
+
+macro_rules! impl_primitive {
+  ($ty:ty, $bytes:expr) => {
+    impl PrimitiveRead for $ty
+    {
+      #[inline]
+      fn read_le<R>(reader: &mut R) -> io::Result<Self>
+      where
+        R: io::Read,
+      {
+        let mut buf: [u8; $bytes] = [0; $bytes];
+        reader.read_exact(&mut buf)?;
+        Ok(<$ty>::from_le_bytes(buf))
+      }
+
+      #[inline]
+      fn read_be<R>(reader: &mut R) -> io::Result<Self>
+      where
+        R: io::Read,
+      {
+        let mut buf: [u8; $bytes] = [0; $bytes];
+        reader.read_exact(&mut buf)?;
+        Ok(<$ty>::from_be_bytes(buf))
+      }
+    }
+
+    impl Read for $ty
+    {
+      #[inline]
+      fn read<R>(reader: &mut R) -> io::Result<Self>
+      where
+        R: io::Read,
+        Self: PrimitiveRead,
+      {
+        Self::read_ne(reader)
+      }
+    }
+  };
+}
+
+impl_primitive!(u8, 1);
+impl_primitive!(u16, 2);
+impl_primitive!(u32, 4);
+impl_primitive!(u64, 8);
+impl_primitive!(u128, 16);
+impl_primitive!(i8, 1);
+impl_primitive!(i16, 2);
+impl_primitive!(i32, 4);
+impl_primitive!(i64, 8);
+impl_primitive!(i128, 16);
+impl_primitive!(f32, 4);
+impl_primitive!(f64, 8);
---
+++ struct-reader-derive/src/lib.rs
@@ -0,0 +1,76 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+/// This derive macro reads the fields of a struct and generates a valid
+/// [`struct_reader::Read::read`] function. All of the field types must implement the
+/// [`struct_reader::Read`] trait. Only named structures are supported at this point, but I am
+/// thinking aboout expanding the supported types.
+#[proc_macro_derive(Read)]
+pub fn derive_read_struct(input: TokenStream) -> TokenStream
+{
+  let ast = parse_macro_input!(input as DeriveInput);
+  let struct_name = &ast.ident;
+
+  // fields of the input struct must be named (at least for now).
+  let fields = if let syn::Data::Struct(syn::DataStruct {
+    fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+    ..
+  }) = ast.data
+  {
+    named
+  } else {
+    panic!("'Read' derive macro only supports structs with named fields.");
+  };
+
+  let read_impl_fields = fields.iter().map(|f| {
+    let field_name = &f.ident;
+    let field_ty = &f.ty;
+
+    // If the field type is an array, extract the length and element type of the array.
+    if let syn::Type::Array(syn::TypeArray {
+      elem,
+      len: syn::Expr::Lit(syn::ExprLit {
+        lit: syn::Lit::Int(len),
+        ..
+      }),
+      ..
+    }) = field_ty
+    {
+      let len = len.base10_parse().unwrap_or_else(|err| {
+        panic!("a parsing error occurred while reading the length of an array: {}", err);
+      });
+      let mut array_fields = Vec::new();
+      for _ in 0..len {
+        array_fields.push(quote! { <#elem as ::struct_reader::Read>::read(reader)? });
+      }
+
+      // Generate array reader.
+      quote! {
+        #field_name: [
+          #(#array_fields,)*
+        ]
+      }
+    } else {
+      // Generate normal field reader.
+      quote! {
+        #field_name: <#field_ty as ::struct_reader::Read>::read(reader)?
+      }
+    }
+  });
+
+  let expanded = quote! {
+    impl ::struct_reader::Read for #struct_name {
+      fn read<R>(reader: &mut R) -> ::std::io::Result<Self>
+      where
+        R: ::std::io::Read
+      {
+        Ok(Self {
+          #(#read_impl_fields,)*
+        })
+      }
+    }
+  };
+
+  expanded.into()
+}
